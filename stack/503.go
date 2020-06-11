4438a1944da8cfe572eee43dfa6868e278228882
pengyj <p15281826276@gmail.com>
2020-06-11 18:13:39 +0800

    add stack

---
+++ stack/503.go
@@ -0,0 +1,12 @@
+package main
+
+func nextGreaterElements(nums []int) []int {
+	length := len(nums)
+	stack := make([]int, 0)
+	res := make([]int, length)
+
+}
+
+func main() {
+	
+}
---
+++ stack/84.go
@@ -0,0 +1,10 @@
+package main
+
+func largestRectangleArea(heights []int) int {
+
+}
+
+
+func main() {
+	
+}
