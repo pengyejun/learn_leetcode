dde932062f97286fda96510ffb77ee5bfea10727
pengyejun <p15281826276@gmail.com>
2020-06-11 23:00:09 +0800

    add stack

--- stack/739.go
+++ stack/739.go
@@ -1,5 +1,22 @@
 package main
 
+func dailyTemperatures(T []int) []int {
+	length := len(T)
+	res := make([]int, length)
+	stack := make([]int, 0)
+
+	for i := length - 1; i >= 0; i-- {
+		for len(stack) > 0 && T[i] >= T[stack[len(stack) - 1]] {
+			stack = stack[:len(stack) - 1]
+		}
+		if len(stack) > 0 && T[i] < T[stack[len(stack) - 1]] {
+			res[i] = stack[len(stack) - 1] - i
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
+
 func main() {
 	
 }
